0f1a6e442e8381d37617aaf1c52cf7c9fac407ca
theyorubayesian <akin.o.oladipo@gmail.com>
2021-11-26 23:39:26 +0000

    corrected and tested training logic

--- oge/train_model.py
+++ oge/train_model.py
@@ -1,5 +1,3 @@
-#TODO: Import your dependencies.
-#For instance, below are some dependencies you might need if you are using Pytorch
 import argparse
 import os
 from argparse import Namespace
@@ -15,9 +13,6 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 import torchvision.models as models
 from torchvision import transforms as T
-
-
-# TODO: Import dependencies for Debugging andd Profiling
 
 MODEL_CHECKPOINT_NAME = "oge_resnet50.pt"
 SPLITS = ["train", "val"]
@@ -36,13 +31,8 @@ def create_data_loaders(
     train_batch_size: int,
     test_batch_size: int,
     transforms: T.Compose, 
-    shuffle: bool, 
-    num_workers: int
+    shuffle: bool
 ) -> Tuple[DataLoader, dict, list]:
-    '''
-    This is an optional function that you may or may not need to implement
-    depending on whether you need to use data loaders or not
-    '''
     fashion_dataset = {
         x: datasets.ImageFolder(
             args.train if x == "train" else args.test,
@@ -54,8 +44,7 @@ def create_data_loaders(
         x: DataLoader(
             fashion_dataset[x], 
             batch_size=train_batch_size if x == "train" else test_batch_size, 
-            shuffle=shuffle, 
-            num_workers=num_workers
+            shuffle=shuffle
         )
         for x in SPLITS
     }
@@ -79,10 +68,6 @@ def get_normalization_stats(dataset):
 
 
 def net(num_output_classes: int = 8):
-    '''
-    TODO: Complete this function that initializes your model
-          Remember to use a pretrained model
-    '''
     model = models.resnet50(pretrained=True)
     for param in model.parameters():
         param.requires_grad = False
@@ -94,11 +79,6 @@ def net(num_output_classes: int = 8):
 
 
 def test(model, test_loader: DataLoader, criterion):
-    '''
-    TODO: Complete this function that can take a model and a 
-          testing data loader and will get the test accuray/loss of the model
-          Remember to include any debugging/profiling hooks that you might need
-    '''
     model.eval()
     running_loss = 0
     correct = 0
@@ -111,9 +91,8 @@ def test(model, test_loader: DataLoader, criterion):
             running_loss += loss.item() * inputs.size(0)
             correct += torch.sum(preds == labels.data).item()
 
-    test_loss = running_loss / len(test_loader)
-    test_acc = correct / len(test_loader)
-    
+    test_loss = running_loss / len(test_loader.dataset)
+    test_acc = 100 * correct / len(test_loader.dataset)
     print(
         "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
             test_loss, correct, len(test_loader.dataset), test_acc
@@ -123,31 +102,26 @@ def test(model, test_loader: DataLoader, criterion):
 
 
 def train(model, train_loader: DataLoader, criterion, optimizer, scheduler, args: Namespace):
-    '''
-    TODO: Complete this function that can take a model and
-          data loaders for training and will get train the model
-          Remember to include any debugging/profiling hooks that you might need
-    '''
     model.train()
 
-    for epoch in args.n_epochs:
+    for epoch in range(args.n_epochs):
         epoch_loss = 0
         correct = 0
         for data, target in train_loader:
             data = data.to(args.device)
             target = target.to(args.device)
 
+            optimizer.zero_grad()
             outputs = model(data)
             loss = criterion(outputs, target)
-            epoch_loss += loss
+            epoch_loss += loss.item() * data.size(0)
             
             loss.backward()
             optimizer.step()
 
             prediction = outputs.argmax(dim=1, keepdim=True)
             correct += prediction.eq(target.view_as(prediction)).sum().item()
-        
-        scheduler.step()    # TODO: Check
+        # scheduler.step()    # TODO: Check
         print(
             f"Epoch: {epoch}: Loss {epoch_loss/len(train_loader.dataset)}, "
             f"Accuracy: {100 * (correct / len(train_loader.dataset))}%"
@@ -156,17 +130,10 @@ def train(model, train_loader: DataLoader, criterion, optimizer, scheduler, args
 
 
 def main(args):
-    '''
-    TODO: Initialize a model by calling the net function
-    '''
     model = net(num_output_classes=args.num_classes)
-    
-    '''
-    TODO: Create your loss and optimizer
-    '''
+
     loss_criterion = nn.CrossEntropyLoss()
     optimizer = SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-    # TODO: Include scheduler in training loop
     scheduler = lr_scheduler.StepLR(
         optimizer, 
         step_size=args.scheduler_step_size, 
@@ -190,14 +157,10 @@ def main(args):
         args.train_batch_size,
         args.test_batch_size,
         transforms=transform_data(mean, std),
-        shuffle=args.shuffle,
-        num_workers=-1,
+        shuffle=args.shuffle
     )
-    '''
-    TODO: Call the train function to start training your model
-    Remember that you will need to set up a way to get training data from S3
-    '''
-    model = train(
+
+    train(
         model,
         dataloaders["train"], 
         loss_criterion, 
@@ -205,14 +168,9 @@ def main(args):
         args
     )
     
-    '''
-    TODO: Test the model to see its accuracy
-    '''
-    test(model, dataloaders["test"], loss_criterion)
-    
-    '''
-    TODO: Save the trained model
-    '''
+    test(model, dataloaders["val"], loss_criterion)
+
+    os.makedirs(args.model_dir, exist_ok=True)
     model_path = os.path.join(args.model_dir, MODEL_CHECKPOINT_NAME)
     torch.save(model.state_dict(), model_path)
 
@@ -223,8 +181,8 @@ if __name__=='__main__':
     # ----
     # Data
     # ----
-    parser.add_argument("--train", default=os.environ["SM_CHANNEL_TRAIN"])
-    parser.add_argument("--test", default=os.environ["SM_CHANNEL_TEST"])
+    parser.add_argument("--train", default=os.getenv("SM_CHANNEL_TRAIN", None))
+    parser.add_argument("--test", default=os.getenv("SM_CHANNEL_TEST", None))
     parser.add_argument("--num_classes", type=int, default=8, help="Number of classes in dataset")
     parser.add_argument("--normalize", type=bool, default=True)
     parser.add_argument("--shuffle", type=bool, default=True)
@@ -237,15 +195,16 @@ if __name__=='__main__':
     parser.add_argument("--n_epochs", type=int, default=5, help="Number of epochs to train for")
     parser.add_argument("--lr", type=float, default=0.001, help="Learning rate")
     parser.add_argument("--momentum", type=float, default=0.9, help="") # TODO
-    parser.add_argument("--scheduler_gamma", type=float, default=0.0)
+    parser.add_argument("--scheduler_gamma", type=float, default=0.1)
     parser.add_argument("-scheduler_step_size", type=int, default=2.0)
-    parser.add_argument("--output_dir", default=os.environ["SM_OUTPUT_DATA_DIR"])
-    parser.add_argument("--model_dir", default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--output_dir", default=os.getenv("SM_OUTPUT_DATA_DIR", None))
+    parser.add_argument("--model_dir", default=os.getenv("SM_MODEL_DIR", None))
     
     # ---
     # GPU
     # ---
-    parser.add_argument("--no_cuda", action="store_true")
+    parser.add_argument("--no_cuda", type=bool, default=False)
+    
     args=parser.parse_args()
     
     args.device = torch.device("cuda:0" if not args.no_cuda and torch.cuda.is_available() else "cpu")
